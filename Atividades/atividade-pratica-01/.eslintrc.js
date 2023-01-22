0fc51115a2b2d62240c8a320df42f9e29bf01e89
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-01-22 00:12:02 -0300

    #1 style: add code style config with eslint

---
+++ Atividades/atividade-pratica-01/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+    'import/extensions': 'off',
+  },
+};
