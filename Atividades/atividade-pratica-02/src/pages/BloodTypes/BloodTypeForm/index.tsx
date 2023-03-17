21dcac1ff853a51826e3487183783f9916f29150
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-03-17 17:07:37 -0300

    feat: create blood type

--- Atividades/atividade-pratica-02/src/pages/BloodTypes/BloodTypeForm/index.tsx
+++ Atividades/atividade-pratica-02/src/pages/BloodTypes/BloodTypeForm/index.tsx
@@ -1,3 +1,6 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+import { serverUrl } from '../../../constants/env'
 import {
   FormContainer,
   InputsContainer,
@@ -8,6 +11,34 @@ import {
 } from './styles'
 
 export function BloodTypeForm() {
+  const [type, setType] = useState('')
+  const [factor, setFactor] = useState('')
+
+  function handleTypeChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setType(event.target.value)
+  }
+
+  function handleFactorChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setFactor(event.target.value)
+  }
+
+  function handleSubmitForm() {
+    if (!type || !factor) {
+      alert('Por favor, preencha todos os campos')
+      return
+    }
+
+    axios
+      .post(`${serverUrl}/blood-type`, { type, factor })
+      .then((response) => {
+        alert('Tipo sanguíneo cadastrado com sucesso')
+      })
+      .catch((error) => {
+        console.error(error)
+        alert('Houve um erro ao cadastrar o tipo sanguíneo')
+      })
+  }
+
   return (
     <BloodTypeFormContainer>
       <TitleContainer>
@@ -15,12 +46,25 @@ export function BloodTypeForm() {
       </TitleContainer>
       <FormContainer>
         <InputsContainer>
-          <input id="type" type="text" placeholder="Tipo" />
-          <input id="factor" type="text" placeholder="Fator" />
+          <input
+            id="type"
+            type="text"
+            placeholder="Tipo"
+            onChange={handleTypeChange}
+          />
+
+          <input
+            id="factor"
+            type="text"
+            placeholder="Fator"
+            onChange={handleFactorChange}
+          />
         </InputsContainer>
 
         <ButtonContainer>
-          <SubmitButton type="submit">Cadastrar</SubmitButton>
+          <SubmitButton type="submit" onClick={handleSubmitForm}>
+            Cadastrar
+          </SubmitButton>
         </ButtonContainer>
       </FormContainer>
     </BloodTypeFormContainer>
--- Atividades/atividade-pratica-02/src/pages/BloodTypes/BloodTypeForm/styles.ts
+++ Atividades/atividade-pratica-02/src/pages/BloodTypes/BloodTypeForm/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const BloodTypeFormContainer = styled.div`
+export const BloodTypeFormContainer = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
