e4aa62e871ff08e680eb9aed8946717ef7487945
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-03-23 12:39:52 -0300

    feat: add register page

--- Projeto/frontend/src/Router.tsx
+++ Projeto/frontend/src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { Home } from './pages/Home'
 import { Login } from './pages/Login'
+import { Register } from './pages/Register'
 
 export function Router() {
   return (
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Route>
     </Routes>
   )
